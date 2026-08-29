fae6db23b85a6810277f2eb15e26f68dc73d2647
Dinesh <dineshbelhekar111@gmail.com>
2026-08-29 22:59:52 +0530

    add custom exception and global exception handler

---
+++ Auth_Service/src/main/java/com/dinesh/Auth_Service/exception/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.dinesh.Auth_Service.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
---
+++ Auth_Service/src/main/java/com/dinesh/Auth_Service/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.dinesh.Auth_Service.exception;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
