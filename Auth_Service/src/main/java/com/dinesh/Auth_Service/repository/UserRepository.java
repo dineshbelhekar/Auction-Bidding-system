a2dff6128774dd756c36c94ea3b2fc06330789a1
Dinesh <dineshbelhekar111@gmail.com>
2026-08-29 22:55:47 +0530

    add signup and login

--- Auth_Service/src/main/java/com/dinesh/Auth_Service/repository/UserRepository.java
+++ Auth_Service/src/main/java/com/dinesh/Auth_Service/repository/UserRepository.java
@@ -4,6 +4,13 @@ import com.dinesh.Auth_Service.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface userRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
 }
