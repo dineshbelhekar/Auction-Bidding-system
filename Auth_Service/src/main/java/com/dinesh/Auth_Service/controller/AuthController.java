97fd3545bb7914303ddb448dca5d0b1a64674892
Dinesh <dineshbelhekar111@gmail.com>
2026-08-30 22:39:28 +0530

    add api for otp generation and verification

--- Auth_Service/src/main/java/com/dinesh/Auth_Service/controller/AuthController.java
+++ Auth_Service/src/main/java/com/dinesh/Auth_Service/controller/AuthController.java
@@ -1,8 +1,12 @@
 package com.dinesh.Auth_Service.controller;
 
 import com.dinesh.Auth_Service.Entity.User;
+import com.dinesh.Auth_Service.request.EmailRequestDTO;
 import com.dinesh.Auth_Service.request.LoginRequestDTO;
+import com.dinesh.Auth_Service.request.OTPRequestDTO;
 import com.dinesh.Auth_Service.service.AuthService;
+import com.dinesh.Auth_Service.service.OTPService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +16,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final OTPService otpService;
 
-    public AuthController(AuthService authService){
+    public AuthController(AuthService authService,
+                          OTPService otpService){
         this.authService = authService;
+        this.otpService = otpService;
     }
 
     @GetMapping("/greetings")
@@ -35,4 +42,14 @@ public class AuthController {
         return ResponseEntity.ok(token);
     }
 
+    @PostMapping("/getotp")
+    public ResponseEntity<HttpStatus> getOTP(@RequestBody EmailRequestDTO emailRequestDTO){
+        otpService.generateOtp(emailRequestDTO.getEmail());
+       return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/verify")
+    public String verifyOtp(@RequestBody OTPRequestDTO otpRequestDTO) {
+        return otpService.validateOtp(otpRequestDTO.getEmail(), otpRequestDTO.getOTP()) ? "OTP Verified Successfully" : "Invalid OTP";
+    }
 }
