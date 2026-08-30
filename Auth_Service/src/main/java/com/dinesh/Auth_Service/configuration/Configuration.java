debefaec970f64e93c28993d1d7735ef944d2be0
Dinesh <dineshbelhekar111@gmail.com>
2026-08-30 22:38:41 +0530

    add configuration for redis

--- Auth_Service/src/main/java/com/dinesh/Auth_Service/configuration/Configuration.java
+++ Auth_Service/src/main/java/com/dinesh/Auth_Service/configuration/Configuration.java
@@ -1,12 +1,14 @@
 package com.dinesh.Auth_Service.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-public class SpringSercurityConfig {
+@org.springframework.context.annotation.Configuration
+public class Configuration {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -14,4 +16,21 @@ public class SpringSercurityConfig {
     }
 
 
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+
+        return template;
+    }
 }
