ce2f96cdaf28bc36e91fde697b1b23919bdbafaf
Dinesh <dineshbelhekar111@gmail.com>
2026-09-01 20:33:23 +0530

    add utility method for jwt verification

---
+++ API_Gateway/src/main/java/com/dinesh/API_Gateway/util/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.dinesh.API_Gateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${app.secret.key}")
+    private String secret;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public Claims validateAndExtractClaims(String token) throws JwtException {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+
+    }
+
+
+}
