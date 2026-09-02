4f72af8785f8a51577cc5b583ef74c82a96c1564
Dinesh <dineshbelhekar111@gmail.com>
2026-09-02 13:42:50 +0530

    add filter jwt filter chain

---
+++ Auction_Service/src/main/java/com/dinesh/Auction_Service/filter/HeaderAuthFilter.java
@@ -0,0 +1,37 @@
+package com.dinesh.Auction_Service.filter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class HeaderAuthFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws ServletException, IOException {
+
+        String userId = request.getHeader("X-User-Id");
+        String username = request.getHeader("X-Username");
+
+        if (userId != null) {
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+            auth.setDetails(username); // stash username here
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        chain.doFilter(request, response);
+    }
+}
